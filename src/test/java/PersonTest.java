31e83547bf11043678f373cce84c1ec7e5f4eae1
Yonkokilasi <yonkokilasi@gmail.com>
2017-05-08 12:44:22 +0300

    Added attributes of name and email and tests for them pass

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,29 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class PersonTest {
+
+    @Test
+    public void person_instantiatesCorrectly_true() {
+        Person testPerson = new Person("Henry", "[email protected]");
+        assertEquals(true, testPerson instanceof Person);
+    }
+    @Test
+    public void getName_personInstantiatesWithName_Henry() {
+        Person testPerson = new Person("Henry", "[email protected]");
+        assertEquals("Henry", testPerson.getName());
+    }
+    @Test
+    public void getEmail_personInstantiatesWithEmail_String() {
+        Person testPerson = new Person("Henry","[email protected]");
+        assertEquals("[email protected]", testPerson.getEmail());
+    }
+    @Test
+    public void equals_returnsTrueIfNameAndEmailAreSame_true() {
+        Person firstPerson = new Person("Henry","[email protected]");
+        Person anotherPerson = new Person("Henry", "[email protected]");
+        assertTrue(firstPerson.equals(anotherPerson));
+    }
+
+
+}
